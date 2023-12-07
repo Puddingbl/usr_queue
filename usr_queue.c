be61b77e2436a6da54b30d9e234c39f7e3842c8a
布丁普林 <puttingbl@outlook.com>
2023-12-07 18:05:11 +0800

    second commit puttingbl v0.0.0

--- usr_queue.c
+++ usr_queue.c
@@ -1,9 +1,9 @@
 #include "usr_queue.h"
 
 /**
-	*¶ÓÁÐ³õÊ¼»¯
-	*@pram q ÐèÒª³õÊ¼»¯µÄ¶ÓÁÐ
-	*			 pArray  ´«¸ø¶ÓÁÐµÄÊý×é
+	*ï¿½ï¿½ï¿½Ð³ï¿½Ê¼ï¿½ï¿½
+	*@pram q ï¿½ï¿½Òªï¿½ï¿½Ê¼ï¿½ï¿½ï¿½Ä¶ï¿½ï¿½ï¿½
+	*			 pArray  ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ðµï¿½ï¿½ï¿½ï¿½ï¿½
 **/
 Q_Status queue_init(Queue_t *q, QElemType *pArray, uint8_t array_size, uint16_t item_size)
 {
@@ -28,7 +28,7 @@ void queue_clear(Queue_t *q)
 }
 
 /**
-	*Èë¶Ó
+	*ï¿½ï¿½ï¿½
 **/
 Q_Status queue_insert(Queue_t *q, QElemType e)
 {
@@ -44,7 +44,7 @@ Q_Status queue_insert(Queue_t *q, QElemType e)
 }
 
 /**
-	*³ö¶Ó
+	*ï¿½ï¿½ï¿½ï¿½
 **/
 Q_Status queue_extract(Queue_t *q, QElemType e)
 {
@@ -60,60 +60,3 @@ Q_Status queue_extract(Queue_t *q, QElemType e)
 	return Q_OK;
 }
 
-///**
-//	*¶ÓÁÐ³õÊ¼»¯
-//	*@pram q ÐèÒª³õÊ¼»¯µÄ¶ÓÁÐ
-//	*			 pArray  ´«¸ø¶ÓÁÐµÄÊý×é
-//**/
-//Q_Status queue_init_data(Queue_data_t *q, QElemDataType *pArray, uint8_t size)
-//{
-//	q->front = 0;
-//	q->rear = 0;
-//	q->data = pArray;
-//	q->size = size;
-//	
-//	return Q_OK;
-//}
-
-//uint8_t queue_length_data(Queue_data_t q)
-//{
-//	return (q.rear + q.size - q.front) % q.size;
-//}
-
-//void queue_clear_data(Queue_data_t *q)
-//{
-//	q->front = 0;
-//	q->rear = 0;
-//}
-
-///**
-//	*Èë¶Ó
-//**/
-//Q_Status queue_insert_data(Queue_data_t *q, QElemDataType e)
-//{
-//	if((q->rear+1)%q->size == q->front)
-//	{
-//		return Q_ERROR;
-//	}
-//	
-//	q->data[q->rear] = e;
-//	q->rear = (q->rear+1) % q->size;
-//	
-//	return Q_OK;
-//}
-
-///**
-//	*³ö¶Ó
-//**/
-//Q_Status queue_extract_data(Queue_data_t *q, QElemDataType *e)
-//{
-//	if(q->front == q->rear)
-//	{
-//		return Q_ERROR;
-//	}
-//	
-//	*e = q->data[q->front];
-//	q->front = (q->front+1)%q->size;
-//	
-//	return Q_OK;
-//}
--- usr_queue.h
+++ usr_queue.h
@@ -34,14 +34,6 @@ typedef struct
 	QElemType *data;    
 } Queue_t;
 
-//typedef struct
-//{
-//	uint8_t front;         // �Ѷ�����ָ��
-//	uint8_t rear;    			 // ��������ָ��
-//	uint8_t count;     		 // �ܹ��ж�������
-//	uint8_t size;
-//	QElemDataType *data;    
-//} Queue_data_t;
 
 /* exported functions ------------------------------------------------------- */
 Q_Status queue_init(Queue_t *q, QElemType *pArray, uint8_t array_size, uint16_t item_size);
@@ -49,12 +41,6 @@ uint8_t  queue_length(Queue_t q);
 void queue_clear(Queue_t *q);
 Q_Status queue_insert(Queue_t *q, QElemType e);
 Q_Status queue_extract(Queue_t *q, QElemType e);
-
-//Q_Status queue_init_data(Queue_data_t *q, QElemDataType *pArray, uint8_t size);
-//uint8_t  queue_length_data(Queue_data_t q);
-//void queue_clear_data(Queue_data_t *q);
-//Q_Status queue_insert_data(Queue_data_t *q, QElemDataType e);
-//Q_Status queue_extract_data(Queue_data_t *q, QElemDataType *e);
 
 #ifdef __cplusplus
 }
